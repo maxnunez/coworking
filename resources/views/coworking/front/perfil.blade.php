b0e5b81ffeb492a7901a8a1a4f586e4bef107ff5
Marcelo Carrasco <marcelo.54sml@gmail.com>
2022-02-21 10:36:54 -0400

    change data validate

--- resources/views/coworking/front/perfil.blade.php
+++ resources/views/coworking/front/perfil.blade.php
@@ -6,19 +6,19 @@
 
 
 <h2 class="d-block ">Mis Productos</h2>
-  @include('coworking.front.Template.perfilTemplate',['items'=>$myProducts, 'file'=>'product_images', 'tag'=>'Productos'])
+  @include('coworking.front.Template.perfilTemplate',['items'=>"@$myProducts, 'file'=>'product_images', 'tag'=>'Productos'])
 <h2 class="d-block">Mis Servicios</h2>
-  @include('coworking.front.Template.perfilTemplate',['items'=>$myServices, 'file'=>'product_images', 'tag'=>'Servicio'])
+  @include('coworking.front.Template.perfilTemplate',['items'=>@$myServices, 'file'=>'product_images', 'tag'=>'Servicio'])
 
 <h2 class="d-block">Mis productos en cambios pendientes</h2>
-  @include('coworking.front.Template.perfilMyChange',['items'=>$pendings, 'file'=>'product_images', 'tag'=>'Pendiente a intercambio'])
+  @include('coworking.front.Template.perfilMyChange',['items'=>@$pendings, 'file'=>'product_images', 'tag'=>'Pendiente a intercambio'])
 
 <h2 class="d-block">Productos en cambio pendientes</h2>
-@include('coworking.front.Template.perfilChange',['items'=>$pendings, 'file'=>'product_images', 'tag'=>'Pendiente a intercambio'])
+@include('coworking.front.Template.perfilChange',['items'=>@$pendings, 'file'=>'product_images', 'tag'=>'Pendiente a intercambio'])
 
 <h2 class="d-block">Mis Productos Cambiados</h2>
-  @include('coworking.front.Template.perfilMyChange',['items'=>$changes, 'file'=>'product_images', 'tag'=>'Cambiado'])
+  @include('coworking.front.Template.perfilMyChange',['items'=>@$changes, 'file'=>'product_images', 'tag'=>'Cambiado'])
 
 <h2 class="d-block">Productos cambiados </h2>
-  @include('coworking.front.Template.perfilChange',['items'=>$changes, 'file'=>'product_images', 'tag'=>'Cambiado'])
+  @include('coworking.front.Template.perfilChange',['items'=>@$changes, 'file'=>'product_images', 'tag'=>'Cambiado'])
 @endsection
