c564a855ca8527d4a64b2f9145676df38c89e511
Marcelo Carrasco <marcelo.54sml@gmail.com>
2022-03-07 22:39:10 -0400

    changes input file

--- resources/views/coworking/front/addProduct.blade.php
+++ resources/views/coworking/front/addProduct.blade.php
@@ -49,7 +49,7 @@
                         <span class="form-control upload">
                             <i aria-hidden="true" class="fa fa-upload" id="B">
                             </i>
-                            <input accept="image/*" id="input-fileB" name="url_img" type="file"  style="font-size:14px"/>
+                            <input class="input-file" accept="image/*" id="input-fileB" name="url_img" type="file"  />
                         </span>
                         {!! $errors->first('url_img', '
                         <p class="help-block">
